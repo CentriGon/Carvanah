6dbdb1aa014d34c631c2980914a978d4c42a2ed4
CentriGon <linholmtorbjorn@gmail.com>
2023-09-08 23:22:48 -0400

    Integrated api ninjas car api and put api data inside the table. Also added function for capitilizing return data properly

--- src/Routes/CarView.js
+++ src/Routes/CarView.js
@@ -1,6 +1,7 @@
 import "../Styles/CarView.css"
 import { useNavigate } from "react-router-dom"
 import { useState } from "react";
+import $ from 'jquery';
 
 
 export const CarView = () => {
@@ -15,28 +16,78 @@ export const CarView = () => {
         "Bugatti Veyron"
     ])
     const [carouselNum, setCarouselNum] = useState(0);
+    const [currentCarInfo, setCurrentCarInfo] = useState(["a", "gas", "17", "lamborghini", "aventador coupe", "2012", "12"]);
 
 
 
 
     const changeCarousel = (shouldForward) => {
+        let newNum = -999;
         if (shouldForward) {
             if (carouselNum + 1 >= carsList.length) {
-                setCarouselNum(0)
+                newNum = 0;
             }
             else {
-                setCarouselNum(prevNum => prevNum + 1);
+                newNum = carouselNum + 1;
             }
         }
         else {
             if (carouselNum - 1 < 0) {
-                setCarouselNum(carsList.length - 1);
+                newNum = carsList.length - 1;
             }
             else {
-                setCarouselNum(prevNum => prevNum - 1);
+                newNum = carouselNum - 1;
             }
         }
+
+        const carModel = (carsNames[newNum].split(" "))[1]
+        $.ajax({
+            method: 'GET',
+            url: 'https://api.api-ninjas.com/v1/cars?model=' + carModel,
+            headers: { 'X-Api-Key': 'xQgxYV+/+ypCz5a9eqn8zw==fiTHAw48PfDVfPdr'},
+            contentType: 'application/json',
+            success: function(result) {
+                setCurrentCarInfo([
+                    capitalizeFirstLetter(result[0].transmission),
+                    capitalizeFirstLetter(result[0].fuel_type),
+                    capitalizeFirstLetter(result[0].highway_mpg),
+                    capitalizeFirstLetter(result[0].make),
+                    capitalizeFirstLetter(result[0].model),
+                    capitalizeFirstLetter(result[0].year),
+                    capitalizeFirstLetter(result[0].cylinders)
+                ])
+            },
+            error: function ajaxError(jqXHR) {
+                console.error('Error: ', jqXHR.responseText);
+            }
+        });
+        setCarouselNum(newNum)
     }
+
+    function capitalizeFirstLetter(input) {
+        // Check if the input is a string
+        if (typeof input === 'string' && input.length > 0) {
+          // Split the input into words using whitespace as the delimiter
+          const words = input.split(/\s+/);
+      
+          // Capitalize each word that starts with a letter
+          const capitalizedWords = words.map((word) => {
+            // Check if the first character is a letter
+            if (/^[a-zA-Z]/.test(word)) {
+              // Capitalize the first letter and concatenate the rest of the string
+              return word.charAt(0).toUpperCase() + word.slice(1);
+            }
+            // If the word doesn't start with a letter, return it as is
+            return word;
+          });
+      
+          // Join the capitalized words back into a single string
+          return capitalizedWords.join(' ');
+        }
+      
+        // If the input is not a string or is empty, return it as is
+        return input;
+      }
 
 
     return <div className="car-view">
@@ -64,31 +115,31 @@ export const CarView = () => {
                     </tr>
                     <tr>
                         <td className="table-right-border"> Transmission </td>
-                        <td> Really good </td>
+                        <td> {currentCarInfo[0]} </td>
                     </tr>
                     <tr>
                         <td className="table-right-border"> Fuel Type </td>
-                        <td> Gas </td>
+                        <td> {currentCarInfo[1]}</td>
                     </tr>
                     <tr>
                         <td className="table-right-border"> Highway MPG </td>
-                        <td> 26 </td>
+                        <td> {currentCarInfo[2]} </td>
                     </tr>
                     <tr>
                         <td className="table-right-border"> Make </td>
-                        <td> Camry </td>
+                        <td> {currentCarInfo[3]} </td>
                     </tr>
                     <tr>
                         <td className="table-right-border"> Model </td>
-                        <td> Toyota </td>
+                        <td> {currentCarInfo[4]} </td>
                     </tr>
                     <tr>
                         <td className="table-right-border"> Year </td>
-                        <td> 1993 </td>
+                        <td> {currentCarInfo[5]} </td>
                     </tr>
                     <tr>
                         <td className="table-right-border bottom-row"> Cylinders </td>
-                        <td className="bottom-row">  4 </td>
+                        <td className="bottom-row"> {currentCarInfo[6]} </td>
                     </tr>
                 </table>
                 <button style={{backgroundColor: "var(--green)"}}> Rent {carsNames[carouselNum]}</button>
